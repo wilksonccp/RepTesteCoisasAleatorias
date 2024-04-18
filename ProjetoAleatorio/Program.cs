140d6bfb7d8bd5eea7e6cd018df31d9112f58e5c
Wilksonccp <wilkson.cc@gmail.com>
2024-04-18 20:17:51 -0300

    estou adinionando um progrmaa aleatório para ser inserido no repositório que criei mais recentemente

---
+++ ProjetoAleatorio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoAleatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+        }
+    }
+}
+
+
