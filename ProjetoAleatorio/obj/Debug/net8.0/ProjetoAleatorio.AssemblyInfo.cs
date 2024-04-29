b79aa2658b127e015e5eabb960db96e532a40ba7
Wilksonccp <wilkson.cc@gmail.com>
2024-04-29 11:06:57 -0300

    tentando commitar mais uma vez

--- ProjetoAleatorio/obj/Debug/net8.0/ProjetoAleatorio.AssemblyInfo.cs
+++ ProjetoAleatorio/obj/Debug/net8.0/ProjetoAleatorio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAleatorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4416b9da049ed27899436b7fd230235585534248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21e16d3ea0f3eafdb7851821c39e95557328f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAleatorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAleatorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
