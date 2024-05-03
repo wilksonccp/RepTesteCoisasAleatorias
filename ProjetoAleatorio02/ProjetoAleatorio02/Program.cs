8327f1f337a3564dbb7579d10b7c3be9d8068f9a
Wilksonccp <wilkson.cc@gmail.com>
2024-05-03 16:30:45 -0300

    enviando todos os arquivos da pasta 'APRENDENDO GIT'

---
+++ ProjetoAleatorio02/ProjetoAleatorio02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoAleatório02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse é meu projeto aleatório para aprender Git");
+        }
+    }
+}
+
+
